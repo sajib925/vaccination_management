4880ff7b3c91a29a01f3946b5d9a92797d508f92
sajib925 <sajibahmed1004@gmail.com>
2024-07-09 16:38:48 +0600

    cors allow

--- vaccination_system/settings.py
+++ vaccination_system/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTH_USER_MODEL = "accounts.CustomUser" 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
