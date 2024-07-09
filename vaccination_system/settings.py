23bcdffa17b12c9d2989a17b30aa56623e165953
sajib925 <sajibahmed1004@gmail.com>
2024-07-09 13:15:54 +0600

    add cors origin

--- vaccination_system/settings.py
+++ vaccination_system/settings.py
@@ -89,11 +89,13 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
     "http://localhost:8000",
+    "https://vaccination-management-c8s4.onrender.com"
 )
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    "https://vaccination-management-c8s4.onrender.com"
 ]
 
 
