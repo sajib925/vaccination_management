b5a7d9e9874e2fe8c8969236b92175752be99d69
sajib925 <sajibahmed1004@gmail.com>
2024-07-10 22:09:35 +0600

    some updated

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-# from django.urls import path
-# from .views import CustomRegisterView
-
-# urlpatterns = [
-#     path('registration/', CustomRegisterView.as_view(), name='custom_register'),
-# ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,1 @@
-# from dj_rest_auth.registration.views import RegisterView
-# from .serializers import CustomRegisterSerializer
 
-# class CustomRegisterView(RegisterView):
-#     serializer_class = CustomRegisterSerializer
